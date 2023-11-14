d8985f828c4a552b89d90f112bd6d068e592bdce
Гадиятов Антон Николаевич <AGadiyatov@IBS.RU>
2023-11-15 00:31:48 +0300

    feat: implemented extractNameFromTemplate function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -469,8 +469,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  let name = value.replace('Hello, ', '');
+  name = name.replace('!', '');
+  return name;
 }
 
 /**
