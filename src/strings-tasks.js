0271206e225c2c70c73384353cf76299a651b9fb
Гадиятов Антон Николаевич <AGadiyatov@IBS.RU>
2023-11-15 00:47:43 +0300

    feat: implemented unbracketTag function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -486,8 +486,21 @@ function extractNameFromTemplate(value) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  let tag = '';
+  let started = false;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === '>') {
+      break;
+    }
+    if (started) {
+      tag += str[i];
+    }
+    if (str[i] === '<') {
+      started = true;
+    }
+  }
+  return tag;
 }
 
 /**
